4ccab9ff5200d6a836886dc5287221d30973cd2a
Akash Akter <AkashAkter>
2022-11-11 22:22:19 +0600

    doctor appointment added

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import fluoride from '../../../assets/images/fluoride.png';
+import cavity from '../../../assets/images/cavity.png';
+import whitening from '../../../assets/images/whitening.png';
+import treatment from '../../../assets/images/treatment.png'
+import Service from './Service';
+import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
+
+const Services = () => {
+
+    const servicesData = [
+        {
+            id: 1,
+            name: 'Fluoride Treatment',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: fluoride
+        },
+        {
+            id: 2,
+            name: 'Cavity Filling',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: cavity
+        },
+        {
+            id: 3,
+            name: 'Teeth Whitening',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: whitening
+        },
+    ]
+    return (
+        <div className='my-32'>
+            <div className='text-center my-10'>
+                <h2 className='text-xl font-bold text-primary uppercase'>Our Services</h2>
+                <p className='text-3xl'>Services We Provide</p>
+            </div>
+            <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    servicesData.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+
+            <div className="hero min-h-screen mt-16 lg:p-20">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={treatment} className="lg:w-1/2 rounded-lg shadow-2xl" alt='' />
+                    <div>
+                        <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                        <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                        <PrimaryButton>Get Started</PrimaryButton>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
